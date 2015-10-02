3dc919fcb161e56cbb8f19aec2911b80fc5892ff
Polo Alcaide, Adrián <apoloa@indra.es>
2015-10-02 15:05:12 +0200

    Added the new test and repair the first because the mocha file not working when use async methods.

--- test/nodepop.js
+++ test/nodepop.js
@@ -25,11 +25,47 @@ var assert = require('assert');
 var request = require('request');
 
 
-describe('Ads', function(){
-    it("Get simple Ads", function(){
-        request('http://localhost:3000/apiV1/ads',function(error,response,body){
-            assert.equals(200,response.statusCode);
-            assert.equals(true, body.success);
+describe('Nodepop Api Test', function(){
+    describe('Ads:', function(){
+        it('Get simple Ads', function(done){
+            assert.doesNotThrow(function(){
+                request('http://localhost:3000/apiV1/ads',function(error,response,body){
+                    assert.equal(200,response.statusCode);
+
+                    done();
+                });
+            });
+        });
+    });
+    describe('Users:', function(){
+        it('Ad new user', function(done){
+            console.log('llamado');
+
+            var postData = {
+                name: 'test',
+                email: 'test@test.com',
+                password: 'holahola'
+            };
+
+            assert.doesNotThrow(function(){
+                console.log("Se llama¿?");
+                request.post({url:'http://localhost:3000/apiV1/users', form: postData}, function(error,response,body){
+                    if(error){
+                        throw new Error(error);
+                    }
+                    console.log("Hecho Request");
+                    console.log('ERROR:',error);
+                    console.log('BODY',body);
+                    console.log('Response', response.statusCode);
+                    assert.equal(201,response.statusCode);
+                    //assert.equal(201,response.statusCode);
+
+                    done();
+                });
+            });
+
+            console.log('Inicializado');
         });
     });
 });
+
